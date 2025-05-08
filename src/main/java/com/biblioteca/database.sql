99490687edebb79b3c3fcd91611b19a803f893b7
Oscar Palomino <oscar_epalomino@soy.sena.edu.co>
2025-05-07 21:18:44 -0500

    SQL modificado

--- src/main/java/com/biblioteca/database.sql
+++ src/main/java/com/biblioteca/database.sql
@@ -6,35 +6,34 @@ USE biblioteca;
 
 -- Tabla para ElementoBiblioteca (tabla base)
 CREATE TABLE ElementoBiblioteca (
-                                    id INT AUTO_INCREMENT PRIMARY KEY,
-                                    titulo VARCHAR(255) NOT NULL,
-                                    autor VARCHAR(255) NOT NULL,
-                                    ano_publicacion INT NOT NULL,
-                                    tipo VARCHAR(50) NOT NULL
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(255) NOT NULL,
+    autor VARCHAR(255) NOT NULL,
+    ano_publicacion INT NOT NULL,
+    tipo VARCHAR(50) NOT NULL
 );
 
 -- Tabla para DVD (hereda de ElementoBiblioteca)
 CREATE TABLE DVD (
-                     id INT PRIMARY KEY,
-                     duracion INT NOT NULL,
-                     genero VARCHAR(100) NOT NULL,
-                     FOREIGN KEY (id) REFERENCES ElementoBiblioteca(id) ON DELETE CASCADE
+     id INT PRIMARY KEY,
+     duracion INT NOT NULL,
+     genero VARCHAR(100) NOT NULL,
+     FOREIGN KEY (id) REFERENCES ElementoBiblioteca(id) ON DELETE CASCADE
 );
 
 -- Tabla para Libro (hereda de ElementoBiblioteca)
 CREATE TABLE Libro (
-                       id INT PRIMARY KEY,
-                       isbn VARCHAR(50) NOT NULL,
-                       numero_paginas INT NOT NULL,
-                       genero VARCHAR(100) NOT NULL,
-                       editorial VARCHAR(255) NOT NULL,
-                       FOREIGN KEY (id) REFERENCES ElementoBiblioteca(id) ON DELETE CASCADE
+   id INT PRIMARY KEY,
+   isbn VARCHAR(50) NOT NULL,
+   numero_paginas INT NOT NULL,
+   genero VARCHAR(100) NOT NULL,
+   editorial VARCHAR(255) NOT NULL,
+   FOREIGN KEY (id) REFERENCES ElementoBiblioteca(id) ON DELETE CASCADE
 );
 
 CREATE TABLE Revista (
-                         id INT PRIMARY KEY,
-                         numero_edicion INT NOT NULL,
-                         categoria VARCHAR(100),
-                         FOREIGN KEY (id) REFERENCES ElementoBiblioteca(id)
+     id INT PRIMARY KEY,
+     numero_edicion INT NOT NULL,
+     categoria VARCHAR(100),
+     FOREIGN KEY (id) REFERENCES ElementoBiblioteca(id)
 );
-
